6761756fb300534871a8b308cbdbf5b6630670a9
muxigame <76636667+muxigame@users.noreply.github.com>
2021-11-24 00:54:32 +0800

    包含图系统中专用的属性
    首次提交包含图系统中专用的属性

---
+++ Graph/Attributes/DefaultValueAttribute.cs
@@ -0,0 +1,26 @@
+﻿//======================================================================
+//
+//       CopyRight 2019-2021 © MUXI Game Studio 
+//       . All Rights Reserved 
+//
+//        FileName :  DefaultValueAttribute.cs
+//
+//        Created by 半世癫(Roc) at 2021-02-04 16:18:24
+//
+//======================================================================
+
+using System;
+
+namespace GalForUnity.Graph.Attributes{
+    /// <summary>
+    /// 指示一个默认值
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class DefaultValueAttribute:Attribute{
+        public object[] value;
+        public DefaultValueAttribute(params object[] value){
+            this.value = value;
+        }
+    }
+    
+}
---
+++ Graph/Attributes/NodeFieldTypeAttribute.cs
@@ -0,0 +1,54 @@
+﻿//======================================================================
+//
+//       CopyRight 2019-2021 © MUXI Game Studio 
+//       . All Rights Reserved 
+//
+//        FileName :  NodeFieldTypeAttribute.cs
+//
+//        Created by 半世癫(Roc)
+//
+//======================================================================
+
+using System;
+using GalForUnity.System;
+using UnityEngine;
+
+namespace GalForUnity.Graph.Attributes{
+    /// <summary>
+    /// 为Node字段提供命名功能和类型支持，记录着一个名称和类型，改类型会在后期由用户自行使用，如给ObjectField提供一个类型，如果不指定类型，默认类型为MonoBehaviour
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Field)]
+    public class NodeFieldTypeAttribute : PropertyAttribute{
+        //NodeFieldTypeAttribute
+        public Type Type;
+        public string Name;
+        
+        public NodeFieldTypeAttribute(Type type, string name){
+            Type = type;
+            Name = GfuLanguage.Parse(name);
+        }
+
+        public NodeFieldTypeAttribute(Type type) : this(type, type.Name){ }
+        public NodeFieldTypeAttribute(string name) : this(typeof(MonoBehaviour), name){ }
+
+        public NodeFieldTypeAttribute() : this(typeof(MonoBehaviour), "对象"){ }
+    }
+    
+    [Flags]
+    public enum NodeAttributeTargets{
+        FlowGraph = 2,
+        ItemGraph = 4
+    }
+
+    /// <summary>
+    /// 允许Node向哪个图系统开放使用
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class NodeAttributeUsage : PropertyAttribute{
+        public NodeAttributeTargets nodeAttributeTargets;
+
+        public NodeAttributeUsage(NodeAttributeTargets nodeAttributeTargets = NodeAttributeTargets.FlowGraph){
+            this.nodeAttributeTargets = nodeAttributeTargets;
+        }
+    }
+}
